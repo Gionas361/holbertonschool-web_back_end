b168ca29f1cffdf1a76dacbdfe87b038becd0d3b
Gionas361 <giovannicarmona57@gmail.com>
2024-01-04 12:55:20 -0400

    int

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -5,6 +5,6 @@ Takes a float variable and returns the floor of it.
 import math
 
 
-def floor(n: float) -> float:
+def floor(n: float) -> int:
     """Returns the floor of n"""
     return (math.floor(n))
