41c2e65f3828936168afadc7b82c61f8f37f5089
Gionas361 <giovannicarmona57@gmail.com>
2024-01-04 11:26:57 -0400

    int -> float and documentation

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
-
-def add(a: int, b: int) -> int:
+"""
+Takes 2 float variables and returns the sum of both of them.
+"""
+def add(a: float, b: float) -> float:
+    """Returns the sum of the 2 floats"""
     return (a + b)
