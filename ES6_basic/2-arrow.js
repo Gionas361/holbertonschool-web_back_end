969f5c2979771938db0fb72dc4d53627d962326f
Gionas361 <giovannicarmona57@gmail.com>
2024-01-16 11:34:02 -0400

    node_modules into respective folder

---
+++ ES6_basic/2-arrow.js
@@ -0,0 +1,11 @@
+export default function getNeighborhoodsList() {
+  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+
+  const self = this;
+  this.addNeighborhood = function add(newNeighborhood) {
+    self.sanFranciscoNeighborhoods.push(newNeighborhood);
+    return self.sanFranciscoNeighborhoods;
+  };
+}
+
+const res = (x) => ['SOMA', 'Union Square'] + (x);
