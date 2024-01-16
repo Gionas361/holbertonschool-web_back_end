e61f88c217ba8c8cad4d9f123d16af2c2d1dca2f
Gionas361 <giovannicarmona57@gmail.com>
2024-01-16 11:57:18 -0400

    in -> of

--- ES6_basic/4-rest-parameter.js
+++ ES6_basic/4-rest-parameter.js
@@ -1,6 +1,6 @@
 export default function returnHowManyArguments(...theArgs) {
   let args = 0;
-  for (const i in theArgs) {
+  for (const i of theArgs) {
     args += 1 + (i - i);
   }
   return args;
