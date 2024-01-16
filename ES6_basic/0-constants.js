76f8c4d9d1c8fc66ff4217dc7f8971ad8bcd1cfe
Gionas361 <giovannicarmona57@gmail.com>
2024-01-16 10:27:53 -0400

    Task one finished

--- ES6_basic/0-constants.js
+++ ES6_basic/0-constants.js
@@ -1,5 +1,6 @@
 export function taskFirst() {
   const task = 'I prefer const when i can.';
+  return task;
 }
 
 export function getLast() {
@@ -7,7 +8,7 @@ export function getLast() {
 }
 
 export function taskNext() {
-  combination = 'But sometimes let';
+  let combination = 'But sometimes let';
   combination += getLast();
 
   return combination;
