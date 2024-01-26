92cea477bbedc5d6747eabeda176938ca4df1819
Gionas361 <giovannicarmona57@gmail.com>
2024-01-26 09:43:26 -0400

    project 0

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,17 +1,11 @@
 #!/usr/bin/node
 function getResponseFromAPI() {
+  // eslint-disable-next-line
   return new Promise((resolve, reject) => {
-    // Simulating an asynchronous operation, like an API call
     setTimeout(() => {
-      const success = true; // Change this based on your actual API response
-
-      if (success) {
-        // Resolve the promise with the desired data
-        resolve({ data: "API response data" });
-      } else {
-        // Reject the promise with an error
-        reject(new Error("API request failed"));
-      }
-    }, 2000); // Simulating a 2-second delay
+      resolve('true');
+      reject(new Error('The fake API is not working currently'));
+    });
   });
 }
+export default  getResponseFromAPI;
