6e26a9cff1e1e6bcadebe8f237b3efed148efbda
Gionas361 <giovannicarmona57@gmail.com>
2024-01-26 21:22:59 -0400

    tasks

---
+++ ES6_promise/9-try.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+export default function guardrail(mathFunction) {
+    const queue = [];
+    try {
+        queue.push(mathFunction());
+    } catch (error) {
+        queue.push(error.toString());
+    }
+    queue.push('Guardrail was processed');
+    return queue;
+}
